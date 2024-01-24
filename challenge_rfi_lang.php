ecd7d92fef0c5a58c6428ceb8f1283a0ff2fc238
xavierfeltin <xavier.feltin@gmail.com>
2024-01-24 09:59:01 +0100

    Update challenge_rfi_lang.php

--- challenge_rfi_lang.php
+++ challenge_rfi_lang.php
@@ -1,4 +1,11 @@
-<?
+<?php
 //RFI challenge
-
+// $dirPath contain path to directory whose files are to be listed 
+$files = scandir($dirPath);  
+foreach ($files as $file) {
+    $filePath = $dirPath . '/' . $file;
+    if (is_file($filePath)) {
+        echo $file . "<br>";
+    }
+}
 ?>
